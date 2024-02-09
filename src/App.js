3320b6e35e880762045050fa231176a6b7671d16
Janine Glaser <Jeannyitb.info@gmail.com>
2024-02-09 16:14:31 +0100

    add-List-Component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import "./App.css";
 import Form from "./components/From";
-import { useState } from "react";
+//    import { useState } from "react";
 import { uid } from "uid";
+import List from "./components/List";
+import useLocalStorageState from "use-local-storage-state";
 
 export default function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useLocalStorageState("activities", {
+    defaultValue: [],
+  });
   function handleAddActivity(newActivities) {
-    setActivities([{ newActivities, id: uid() }, ...activities]);
+    setActivities([{ ...newActivities, id: uid() }, ...activities]);
   }
 
   return (
     <div className="App">
       <header className="header"></header>
       <Form onAddActivity={handleAddActivity} />
+      <List activities={activities} />
     </div>
   );
 }

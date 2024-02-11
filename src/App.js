fcc841d725edce76a6bdd6b38cb0913f35467970
Janine Glaser <Jeannyitb.info@gmail.com>
2024-02-11 16:51:52 +0100

    add filtering the list

--- src/App.js
+++ src/App.js
@@ -9,6 +9,23 @@ export default function App() {
   const [activities, setActivities] = useLocalStorageState("activities", {
     defaultValue: [],
   });
+  const isGoodWeather = true;
+  const isBadWeather = false;
+
+  function filteredGoodWeatherActivities(isGoodWeather) {
+    return activities.filter((activity) => {
+      if (activity.forNiceWeather === isGoodWeather) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+  }
+
+  const filteredBadWeatherActivities = activities.filter(
+    (activity) => activity.forNiceWeather === isBadWeather
+  );
+
   function handleAddActivity(newActivities) {
     setActivities([{ ...newActivities, id: uid() }, ...activities]);
   }
@@ -17,7 +34,19 @@ export default function App() {
     <div className="App">
       <header className="header"></header>
       <Form onAddActivity={handleAddActivity} />
-      <List activities={activities} />
+      {/* <List activities={activities} /> */}
+
+      <h4> Bad weather activities </h4>
+      <List
+        activities={filteredBadWeatherActivities}
+        isGoodWeather={isBadWeather}
+      />
+      <h4> Nice weather activities </h4>
+      <List
+        activities={filteredGoodWeatherActivities(isGoodWeather)}
+        is
+        isGoodWeather={isGoodWeather}
+      />
     </div>
   );
 }

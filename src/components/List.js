960f2ad3b2deddb52911e4eb2800f0f98d1cdead
Janine Glaser <Jeannyitb.info@gmail.com>
2024-02-12 10:47:26 +0100

    add delete an item

--- src/components/List.js
+++ src/components/List.js
@@ -1,10 +1,15 @@
 import "./List.css";
 
-export default function List({ activities }) {
+export default function List({ activities, onDeleteActivity }) {
   return (
     <ul>
       {activities.map((activity) => (
-        <li key={activity.id}>...{activity.name} </li>
+        <li key={activity.id}>
+          ...{activity.name}
+          <button type="button" onClick={() => onDeleteActivity(activity.id)}>
+            ✗
+          </button>
+        </li>
       ))}
     </ul>
   );
